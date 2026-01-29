eeaf7ae8da8e59d286b0097abd35b039e382b295
Dylan Neff <Dyn0402@hotmail.com>
2026-01-29 19:37:54 +0100

    processor_test.py now run overnight run

--- processor_test.py
+++ processor_test.py
@@ -28,8 +28,8 @@ def main():
     # processor = Processor('/local/home/banco/dylan/out_dir/Run/night_test_non_zs_config2-2_10-20-25/run_config.json')
     # processor = Processor('/mnt/cosmic_data/Run/rd5_grid_vfp_1_co2_fe55_zs2_10-21-25/run_config.json')
     # processor = Processor('/mnt/cosmic_data/Run/rd5_strip_esl_1_co2_cosmics_10-27-25/run_config.json')
-    # processor = Processor('/data/cosmic_data/Run_MX/mx17_det1_overnight_run_1-27-26/run_config.json')
-    processor = Processor('/data/cosmic_data/Run_MX/mx17_det1_daytime_run_1-28-26/run_config.json')
+    processor = Processor('/data/cosmic_data/Run_MX/mx17_det1_overnight_run_1-27-26/run_config.json')
+    # processor = Processor('/data/cosmic_data/Run_MX/mx17_det1_daytime_run_1-28-26/run_config.json')
     # processor = Processor('/mnt/beam_test_25_data/rd5_grid_vfp_1_co2_10-21-25/run_config.json')
     # processor = Processor('/local/home/banco/dylan/out_dir/Run/night_test_non_zs_config2-2_10-23-25/run_config.json')
     # processor.config['save_fds'] = True
